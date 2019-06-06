92bd8dd66c667c99993129a2816fff10f098b4d1
Daniel <979223119@qq.com>
2019-06-06 20:29:10 +0800

    2019.6.6

---
+++ 14181. Flying Safely/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int n, m, a, b;
+        cin >> n >> m;
+        while(m--){
+            cin >> a >> b;
+        }
+        cout << n - 1 << endl;
+    }
+    return 0;
+}
